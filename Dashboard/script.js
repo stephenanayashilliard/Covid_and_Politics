775063ab7b5090008e4b20630b5034f8d7908a72
Stephen Anayas Hilliard <stephenanayashilliard@gmail.com>
2021-03-09 11:51:31 -0800

    updates on index and script

---
+++ Dashboard/script.js
@@ -0,0 +1,14 @@
+d3.json("covid.json", function(data) {
+    console.log(data);
+});
+
+d3.selectAll("body").on("change", updatePage);
+
+function updatePage() {
+  var dropdownMenu = d3.selectAll("#selectOption").node();
+  var dropdownMenuID = dropdownMenu.id;
+  var selectedOption = dropdownMenu.value;
+
+  console.log(dropdownMenuID);
+  console.log(selectedOption);
+};
