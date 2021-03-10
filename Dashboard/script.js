033a07119d4af31ff1fa08f583298b3df146db40
Stephen Anayas Hilliard <stephenanayashilliard@gmail.com>
2021-03-09 19:52:10 -0800

    needed to add object values

--- Dashboard/script.js
+++ Dashboard/script.js
@@ -1,13 +1,13 @@
 function init() {
     //Grab a reference to teh dropdown select element
-    var selector = d3.select("selDataset");
+    var selector = d3.select("#selDataset");
 
 // Use the list of sample names to populate the select options
 d3.json("covid.json").then((data) => {
     console.log(data);
     var sampleNames = data.county_state;
 
-    sampleNames.forEach((sample) => {
+    Object.values(sampleNames).forEach((sample) => {
       selector
         .append("option")
         .text(sample)
