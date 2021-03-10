9673f3a756e7ca4cd6658439d53e3938d071d019
Stephen Anayas Hilliard <stephenanayashilliard@gmail.com>
2021-03-10 07:49:03 -0800

    Demographics Panel

--- Dashboard/script.js
+++ Dashboard/script.js
@@ -1,5 +1,34 @@
+// Demographics Panel 
+function buildMetadata(sample) {
+
+    d3.json("covid.json").then((data) => {
+      var metadata = data.county_state; data.totalpop; data.income; data.cases_per_capita_100k;
+      data.deaths_per_capita_100k; data.pop_per_sq_mile_2010
+
+      // Filter the data for the object with the desired sample number
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      // Use d3 to select the panel with id of `#sample-metadata`
+      var PANEL = d3.select("#sample-metadata");
+  
+      // Use `.html("") to clear any existing metadata
+      PANEL.html("");
+  
+      // Use `Object.entries` to add each key and value pair to the panel
+      // Hint: Inside the loop, you will need to use d3 to append new
+      // tags for each key-value in the metadata.
+      Object.entries(result).forEach(([key, value]) => {
+        PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+      });
+  
+    });
+  }
+
+
+// Drop down Menu
+
 function init() {
-    //Grab a reference to teh dropdown select element
+    //Grab a reference to the dropdown select element
     var selector = d3.select("#selDataset");
 
 // Use the list of sample names to populate the select options
