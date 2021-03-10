27772d849aaca8a916cce3c6ad5178fbd69fcec7
Melyssa Sibal <melyssa.sibal@gmail.com>
2021-03-09 20:41:03 -0500

    uploading results for lm

--- Race Covid Analysis/race_covid_analysis.R
+++ Race Covid Analysis/race_covid_analysis.R
@@ -3,7 +3,7 @@ library(readr)
 covid_and_politics_db <- read_csv("Database_Model/Final_Database/covid_and_politics_db.csv")
 library(dplyr)
 library(tidyverse)
-library(ggplot2)
+library(ggplot2)ggplot(covid_and_politics_db, aes(x=white)) + geom_density()
 
 # race is categorized as hispanic, black, white, native, asian, pacific in data, represented by population percentage
 # visualize distribution using density plot
@@ -12,4 +12,9 @@ ggplot(covid_and_politics_db, aes(x=black)) + geom_density()
 ggplot(covid_and_politics_db, aes(x=native)) + geom_density()
 ggplot(covid_and_politics_db, aes(x=asian)) + geom_density()
 ggplot(covid_and_politics_db, aes(x=pacific)) + geom_density()
-ggplot(covid_and_politics_db, aes(x=white)) + geom_density()
+
+# linear regression model 
+lm(deaths ~ hispanic + black + native + asian + pacific + white,data=covid_and_politics_db)
+lm(cases ~ hispanic + black + native + asian + pacific + white,data=covid_and_politics_db)
+summary(lm(deaths ~ hispanic + black + native + asian + pacific + white,data=covid_and_politics_db))
+summary(lm(cases ~ hispanic + black + native + asian + pacific + white,data=covid_and_politics_db))
