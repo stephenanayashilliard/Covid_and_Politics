008485b622a197af23a03364fc6b653a0e73d103
Stephen Anayas Hilliard <stephenanayashilliard@gmail.com>
2021-03-09 17:29:23 -0800

    trying to fix json file

--- Dashboard/script.js
+++ Dashboard/script.js
@@ -1,7 +1,9 @@
 
 
 // Use the list of sample names to populate the select options
+function init() {
 d3.json("covid.json").then((data) => {
+    console.log(data);
     var sampleNames = data.county_state;
 
     sampleNames.forEach((sample) => {
@@ -10,5 +12,7 @@ d3.json("covid.json").then((data) => {
         .text(sample)
         .property("value", sample);
   });
+})}
 
-})
+init();
+
