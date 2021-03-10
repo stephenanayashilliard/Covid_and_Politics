b3519908f574e6eedce40972dd64d7d7e5cb8ed8
Stephen Anayas Hilliard <stephenanayashilliard@gmail.com>
2021-03-09 18:00:24 -0800

    adjustment to script

--- Dashboard/script.js
+++ Dashboard/script.js
@@ -1,7 +1,8 @@
-
+function init() {
+    //Grab a reference to teh dropdown select element
+    var selector = d3.select("selDataset");
 
 // Use the list of sample names to populate the select options
-function init() {
 d3.json("covid.json").then((data) => {
     console.log(data);
     var sampleNames = data.county_state;
